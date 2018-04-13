47aaf6dd01d7b92d5855178f54e4fef8f6b47c52
Dima Romanovskiy <Dmitriy_r_f@mail.ru>
2018-04-13 19:17:50 +0300

    my page without my description

--- assets/scripts/application.js
+++ assets/scripts/application.js
@@ -62,3 +62,29 @@ window.onload = function() {
 
 
 })();
+
+
+
+jQuery(document).ready(function($){
+
+	$('#carouseltechnologiesSlides').carousel({
+	  interval: 2400
+	})
+
+	$('.carousel .carousel-item').each(function(){
+	  var next = $(this).next();
+	  if (!next.length) {
+	    next = $(this).siblings(':first');
+	  }
+	  next.children(':first-child').clone().appendTo($(this));
+	  
+	  if (next.next().length>0) {
+	    next.next().children(':first-child').clone().appendTo($(this));
+	  }
+	  else {
+	  	$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+	  }
+	});
+
+
+});
